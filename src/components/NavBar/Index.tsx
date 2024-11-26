21d6ef32d4264a6886d933381d37b68c8c36f2db
Rossignol Alex <alexnw33910@gmail.com>
2024-11-26 23:03:10 +0100

    NavBar ✅

---
+++ src/components/NavBar/Index.tsx
@@ -0,0 +1,30 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import logoM from "@public/logoM.svg";
+import "@/components/NavBar/navbar.css";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <header>
+      <div className="header_work">
+        <Link to={"/"} className="work">
+          <h2>Work</h2>
+        </Link>
+      </div>
+      <button type="button" onClick={() => navigate("/")}>
+        <img src={logoM} alt="Marlene Ayrault" />
+      </button>
+      <div className="header_about">
+        <a
+          href="/64ec4fea469d6a03a2acd4db_CV-MARLENE_AYRAULT.pdf"
+          download="Marlene_Ayrault_CV.pdf"
+          className="about"
+        >
+          <h2>About</h2>
+        </a>
+      </div>
+    </header>
+  );
+}
